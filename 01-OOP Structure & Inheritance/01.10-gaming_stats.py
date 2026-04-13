4f2b1a83c9635abed52f73ac173aef72ecdfe0ba
Oli Dare <oli.dare.dev@gmail.com>
2026-04-13 21:49:37 +0200

    feat(gaming): implement secure high-score validation

---
+++ 01-OOP Structure & Inheritance/01.10-gaming_stats.py
@@ -0,0 +1,21 @@
+class LeaderBoard:
+    def __init__(self, score):
+        self.score = score
+
+    @property
+    def score(self):
+        return self._score
+
+    @score.setter
+    def score(self, score_value):
+        if 0 <= score_value <= 1000000:
+            self._score = score_value
+        else:
+            raise ValueError("Invalid Score!")
+
+
+p1 = LeaderBoard(500)
+# p1.score = 1500000 would not work
+print(p1.score)
+
+
